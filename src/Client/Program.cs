96c8ca09cc827e3a2504828afe38dc525a1d69e3
wilker.santos <wilker.santos@brktecnologia.com.br>
2023-10-25 10:40:21 -0300

    Adicionar arquivos de projeto.

---
+++ src/Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using IdentityModel.Client;
+using System.Text.Json;
+
+// descobre os endpoints a partir do metadata
+var client = new HttpClient();
+
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5002");
+
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+// request token
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new
+ ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.AccessToken);
+
+Console.WriteLine("\n\n");
+
+// call api
+var apiClient = new HttpClient();
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("https://localhost:6001/identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+else
+{
+    var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
+    Console.WriteLine(JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true }));
+}
+
+Console.WriteLine("\n\n ########### Processamento Concluído   ##############\n");
+Console.ReadKey();
---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,35 @@
+﻿using Duende.IdentityServer.Models;
+namespace IdentityServer;
+public static class Config
+{
+    //https://localhost:5002/.well-known/openid-configuration
+    public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+    {
+            new IdentityResources.OpenId()
+    };
+
+    public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+    {
+            new ApiScope(name: "api1", displayName: "MinhaAPI")
+    };
+
+    public static IEnumerable<Client> Clients =>   new List<Client>
+    {
+        new Client
+        {
+            ClientId = "client",
+
+            // usuário não iterativo, usa o clientid/secret para autenticacao
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // segredo para autenticacao
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+
+            // escopos que o cliente pode acessar
+            AllowedScopes = { "api1" }
+        }
+    };
+}
